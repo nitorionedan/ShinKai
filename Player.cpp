bef9f6f788d593801a6a5e93a71aed4de34f7983
nitorionedan <b1444263@planet.kanazawa-it.ac.jp>
2016-07-05 22:55:24 +0900

    fix player bug

--- Player.cpp
+++ Player.cpp
@@ -13,6 +13,7 @@ Player::Player()
 	, img(new Image)
 {
 	img->Load("Images/test_player.png", "player");
+	Initialize();
 }
 
 
@@ -23,7 +24,7 @@ Player::~Player()
 
 void Player::Initialize()
 {
-	pos.SetVecor2D(160., 120.);
+	pos.SetVecor2D(0., 0.);
 }
 
 
@@ -36,22 +37,25 @@ void Player::Update()
 void Player::Draw()
 {
 	img->DrawRota(pos.x, pos.y, 1., 0., "player", true);
+
+	// TEST
+//	DrawFormatString(0, 30, GetColor(255, 0, 0), "pos.x = %lf", pos.y);
 }
 
 
 void Player::Move()
 {
 	// use gravity
-//	pos.y += GRAVITY;
+	pos.y += GRAVITY;
 
 	// moving
-	if (Keyboard_Get(KEY_INPUT_RIGHT) >= 1)	pos.x + MAX_SPEED;
-	if (Keyboard_Get(KEY_INPUT_LEFT) >= 1)	pos.x - MAX_SPEED;
-	if (Keyboard_Get(KEY_INPUT_DOWN) >= 1)	pos.y + MAX_SPEED;
-	if (Keyboard_Get(KEY_INPUT_UP) >= 1)	pos.y - MAX_SPEED;
+	if (Keyboard_Get(KEY_INPUT_RIGHT) >= 1)	pos.x += MAX_SPEED;
+	if (Keyboard_Get(KEY_INPUT_LEFT) >= 1)	pos.x -= MAX_SPEED;
+	if (Keyboard_Get(KEY_INPUT_DOWN) >= 1)	pos.y += MAX_SPEED;
+	if (Keyboard_Get(KEY_INPUT_UP) >= 1)	pos.y -= MAX_SPEED;
 
 	// over boundary
-	pos.y = std::max(std::min(pos.y, 0.), UNDER_BOUNDARY);
+	pos.y = std::max(std::min(pos.y, UNDER_BOUNDARY), 0.);
 }
 
 // EOF
