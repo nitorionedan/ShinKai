fff9a3d3f7fbed2e9dd2e6e1f0ecb6aa05065187
nitorionedan <b1444263@planet.kanazawa-it.ac.jp>
2016-07-05 17:45:05 +0900

    a little fix in Image class

--- Include/Image.hpp
+++ Include/Image.hpp
@@ -23,6 +23,16 @@ public:
 	int DrawRect(const int& DestX, const int& DestY,
 		const int& SrcX, const int& SrcY, const int& Width, const int& Height,
 		std::string key, const int& TransFlag = 0, const int& TurnFlag = 0);
+
+	/*
+	@brief					Near Raster Scrole
+	@param[in]	X, Y		Position
+	@param[in]	Cycle		Period
+	@param[in]	Shake		Amplitude
+	@param[in]	Key			Key Word
+	@param[in]	isVertical	Does Wave direct vertical or horizontal?
+	@return					Successe >> 1, Failed >> -1
+	*/
 	int DrawRasterScroll(const int &X, const int &Y, double Cycle, double Shake, const std::string &Key, bool isVertical = true);
 
 	int GetSize(std::string key, int* width, int* height);
@@ -30,9 +40,9 @@ public:
 private:
 	int GetHandle(std::string key);
 
-	std::vector<std::string> key;	// キーネーム
-	std::vector<int> gh;			// グラフィックハンドル
-	int count;
+	std::vector<std::string> key;	// key name
+	std::vector<int> gh;			// graphic handle
+	int count;						// loaded file num
 };
 
 
